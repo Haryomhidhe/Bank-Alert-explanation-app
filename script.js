4d148f78f67faa6220fe832689b913982b04d49d
haryomhidhegoshen@gmail.com <haryomhidhegoshen@gmail.com>
2026-06-17 14:58:32 +0100

    new changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Alert data for different types
+const checkBtn = document.querySelector("#checkBtn")
+const Alert = document.querySelector("#alertDetail")
+
+checkBtn.addEventListener('click', function(){
+  let userInput = Alert.value;
+})
+async function loadCharges() {
+  let response = await fetch('app.json');
+  let charges = await response.json();
+  // charges is now your array — usable here
+}
